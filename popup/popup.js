0e998409cff3a20ada07123d9d179474b7dca107
saidatta1 <b118051@iiit-bh.ac.in>
2020-05-23 20:23:56 +0530

    style change

--- popup/popup.js
+++ popup/popup.js
@@ -1,38 +1,38 @@
-"use strict";
+'use strict';
 
-console.log("in popup");
-window.addEventListener("load", async () => {
-  console.log("in load");
-  let list = document.getElementById("list");
-  let addons = await browser.management.getAll();
-  for (let addon of addons) {
-    if (addon.type !== "extension" || !addon.enabled) { continue; }
-    console.log(addon);
+console.log('in popup');
+window.addEventListener('load', async () => {
+	console.log('in load');
+	let list = document.getElementById('list');
+	let addons = await browser.management.getAll();
+	for (let addon of addons) {
+		if (addon.type !== 'extension' || !addon.enabled) {
+			continue;
+		}
+		console.log(addon);
 
-    ( ({name, id}) => {
-      if(name != "extension logging") {
-        let item = document.createElement("li");
-        item.innerText = name;
-        item.addEventListener("click", () => {
-          browser.tabs.create({
-            url: `${browser.runtime.getURL("log/log.html")}#${id}`,
-          });
-          window.close();
-        });
-        list.appendChild(item);
-      }
-    }) (addon);
-  }
+		(({ name, id }) => {
+			if (name != 'extension logging') {
+				let item = document.createElement('li');
+				item.innerText = name;
+				item.addEventListener('click', () => {
+					browser.tabs.create({
+						url: `${browser.runtime.getURL('log/log.html')}#${id}`
+					});
+					window.close();
+				});
+				list.appendChild(item);
+			}
+		})(addon);
+	}
 
-  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-  gettingActiveTab.then((tabs) => {
-    console.log("insidequery");
-    let currentTab = tabs[0];
-    let tabid = currentTab.id;
-    // console.log(currentTab.id);
-    let para = document.getElementById("para");
-    para.innerText = `Current TabId: ${tabid}`;
-  });
+	var gettingActiveTab = browser.tabs.query({ active: true, currentWindow: true });
+	gettingActiveTab.then((tabs) => {
+		console.log('insidequery');
+		let currentTab = tabs[0];
+		let tabid = currentTab.id;
+		// console.log(currentTab.id);
+		let para = document.getElementById('para');
+		para.innerText = `Current Tab Id : ${tabid}`;
+	});
 });
-
-
